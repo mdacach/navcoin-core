94575b73f92d4758bbc6ccb5c37498b6d74c4ab3
Matheus Cardoso <dacachmatheus@gmail.com>
2022-04-28 19:15:43 -0300

    Add example file to test curl and user input

---
+++ bash_curl_input_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This script will install the packages and libraries required to build the NavCoin Core wallet dependancies"
+echo "Do you wish to clone 'NAVCoin/navcoin-core' and build the dependancies for the master branch as well?"
+
+read -p 'Enter Y for yes or anything else to decline: ' uservar
+
+if [ $uservar == "Y" ]
+then
+  echo "The script will clone NavCoin Core and attempt to build the depends"
+else
+  echo "The script will not clone NavCoin Core"
+fi
